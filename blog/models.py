decee8bff3dcf73231d30e0addcfec538868ecbb
Prakhar <44711005+Prag066@users.noreply.github.com>
2020-03-04 16:38:39 +0530

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from app.models import MyUser
+from django.urls import reverse
+
+STATUS = ((0,"Draft"),(1,"Publish"))
+class Post(models.Model):
+    title = models.CharField(max_length=200,unique=True)
+    slug = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(MyUser,on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS,default=0)
+    # upload = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    class Meta:
+        ordering=['-created_on']
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'slug': self.slug})
+
